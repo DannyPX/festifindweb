3034da514e97754d431b184ca6ed49eeb82a1ba2
DannyPX <Damnoga@hotmail.com>
2020-05-19 13:21:35 +0200

    faceboook created fix

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,48 +7,45 @@
   </div>
 </template>
 <script>
-
 export default {
-  name:"facebookLogin",
-  methods: {
-    async logInWithFacebook() {
-      await this.loadFacebookSDK(document, "script", "facebook-jssdk");
-      await this.initFacebook();
-      window.FB.login(function(response) {
-        if (response.authResponse) {
-          alert("You are logged in &amp; cookie set!");
-          // Now you can redirect the user or do an AJAX request to
-          // a PHP script that grabs the signed request from the cookie.
-        } else {
-          alert("User cancelled login or did not fully authorize.");
+    name:"facebookLogin",
+    created: function () {
+        window.fbAsyncInit = function() {
+            window.FB.init({
+                appId: "210799246921372", //You will need to change this
+                cookie: true, // This is important, it's not enabled by default
+                autoLogAppEvents : true,
+                xfbml : true,
+                version: "v13.0"
+            })
+
+            window.FB.getLoginStatus(function(response) {
+                console.log(response)
+            })
         }
-      });
-      return false;
+
+        (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
     },
-    async initFacebook() {
-      window.fbAsyncInit = function() {
-        window.FB.init({
-          appId: "8220179XXXXXXXXX", //You will need to change this
-          cookie: true, // This is important, it's not enabled by default
-          version: "v13.0"
-        });
-      };
-    },
-    async loadFacebookSDK(d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    },
-    async redirectToRegister() {
-      
+    methods: {
+        logInWithFacebook () {
+            window.FB.login(function(response) {
+                if (response.authResponse) {
+                alert("You are logged in &amp; cookie set!");
+                // Now you can redirect the user or do an AJAX request to
+                // a PHP script that grabs the signed request from the cookie.
+                } else {
+                alert("User cancelled login or did not fully authorize.");
+                }
+            });
+            return false;
+        }
     }
-  }
 };
 </script>
 <style>
