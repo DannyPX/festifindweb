8ee569aa9f79a19eeb29884453396f0c7a06be94
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-05-19 13:44:53 +0200

    Update Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,23 +3,23 @@
  <form>
   <div class="form-group">
     <label for="exampleInputEmail1">E-mailaddress</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <small id="emailHelp" class="form-text text-muted">We delen je email met niemand.</small>
   </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Voor-en achternaam</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <small id="emailHelp" class="form-text text-muted">Dit is hoe mensen je op Festifind zien.</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Wachtwoord</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" class="form-control" id="exampleInputPassword1">
     <small id="emailHelp" class="form-text text-muted">Wachtwoord moet bestaan uit minimaal 8 tekens</small>
     <small id="emailHelp" class="form-text text-muted">en een hoofdletter.</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Wachtwoord bevestigen</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
   <div class="form-group form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
