1782a33cdd5b7e6f24645f87569ed4eb880ada47
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-05-20 11:12:51 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,41 @@
 <template>
 <div id="content">
-  <h2 class="subtitle">Populaire events</h2>
+  <h1 class="title">Home</h1>
+
+
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="" alt="Third slide">
+    </div>
   </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
 </template>
 
 <script>
 </script>
 
-<style scoped>
-.subtitle {
-  position: absolute;
-  width: 207.77px;
-  height: 55.55px;
-  left: 161px;
-  top: 85px;
-}
-
+<style>
+  .event-container 
+  {
+    width: 140px;
+    height: 180px;
+    background-color: white;
+    margin: 8px;
+  }
 </style>
