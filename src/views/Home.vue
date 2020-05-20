3707e2dd76cccbd3d07f97e09c2f0850fd92a425
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-05-20 13:29:14 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,36 @@
 
 
   <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <h5 style="text-align: center;">Populaire events</h5>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="" alt="First slide">
+      <div class="event-container">
+        <div class="event-title">
+          <p>Intents</p>
+        </div>
+      </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="" alt="Second slide">
+      <div class="event-container">
+        <div class="event-title">
+          <p>Supremacy</p>
+        </div>
+        
+      </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="" alt="Third slide">
+      <div class="event-container">
+        <div class="event-title">
+          <p>Tomorrowland</p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="event-container">
+        <div class="event-title">
+          <p>Lowlands</p>
+        </div>
+      </div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -24,18 +45,79 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+<div class="home-nav">
+  <div class="home-nav-inner">
+    <router-link :to="{name: 'Calender'}">
+      <button class="home-btn"><i class="fa fa-calendar"></i> kalender</button>
+    </router-link>
+    <br />
+        <router-link :to="{name: ''}">
+      <button class="home-btn"><i class="fas fa-street-view"></i> Dichtbij</button>
+    </router-link>
+    <br />
+  </div>
+</div>
 </div>
 </template>
 
 <script>
+
 </script>
 
 <style>
   .event-container 
   {
-    width: 140px;
+    width: 80%;
+    margin-left: 10%;
+    margin-bottom: 10%;
+    margin-top: 4%;
     height: 180px;
     background-color: white;
-    margin: 8px;
+    color: #333;
+    border-radius: 8px;
+    box-shadow: 6px 6px 4px 2px rgba(0, 0, 255, .1);
+    position: relative;
+  }
+
+  .event-title
+  {
+    position: absolute; 
+    bottom: 0; 
+    width: 100%; 
+    height: 50px; 
+    background-color: white;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    text-align: center;
+    font-family: 'Nunito Regular';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+  }
+ 
+  .home-nav
+  {
+    margin-top: 25%;
+  }
+
+  .home-nav-inner
+  {
+    width: 180px;
+    margin: auto;
+  }
+
+  .home-btn
+  {   
+    color: white;
+    text-align: left;
+    font-family: 'Nunito Regular';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 32px;
+    width: 100%;
+  }
+  .home-btn a 
+  {
+    color: white;
   }
 </style>
