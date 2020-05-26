e4aa0cee716a647363457ba37951ea69cdcad966
Jim Spijker <409996@student.fontys.nl>
2020-05-26 13:27:37 +0200

    Festival Landingpage

--- src/views/Festival.vue
+++ src/views/Festival.vue
@@ -1,64 +1,102 @@
 <template>
   <div id="content">
-    <img class="festivalImage" src="../assets/maxresdefault.jpg" />
-    <h3 class="festivalName">Awakenings Festival 2020</h3>
-    <button class="btn">Tickets</button>
-    <button class="btn btn2">Ik ga</button>
-    <h6 class="heading">12 vrienden gaan ></h6>
-    <img
-      src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
-      class="profilePicture"
-    />
-    <img
-      src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
-      class="profilePicture"
-    />
-    <img
-      src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
-      class="profilePicture"
-    />
-    <img
-      src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
-      class="profilePicture"
-    />
-    <img
-      src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
-      class="profilePicture"
-    />
-    <img
-      src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
-      class="profilePicture"
-    />
-    <img
-      src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
-      class="profilePicture"
-    />
-    <img
-      src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
-      class="profilePicture"
-    />
-    <img
-      src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
-      class="profilePicture"
-    />
-    <!-- More than 10 friends: show the first 9 and the counter underneath -->
-    <div class="maxFriends">
-      <p class="maxFriendsCounter">+12</p>
+    <div class="innercontent">
+      <a>
+        <i class="fas fa-arrow-left fa-3x backIcon"></i>
+      </a>
+      <img class="festivalImage" src="../assets/maxresdefault.jpg" />
+      <h3 class="festivalName">Awakenings Festival 2020</h3>
+      <button class="btn">Tickets</button>
+      <!-- Check if user is going to festival And edit value accordingly -->
+      <input type="button" class="btn btn2" id="btn2" value="Ik ga" @click="change">
+      <h6 class="heading">12 vrienden gaan</h6>
+      <!-- Per friend -->
+      <img
+        src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
+        class="profilePicture"
+      />
+
+      <!-- More than 10 friends: show the first 9 and the counter underneath -->
+      <div class="maxFriends">
+          <!-- Amount of friends -9-->
+        <p class="maxFriendsCounter">+3</p>
+      </div>
+
+      <h6 class="heading">Info</h6>
+
+      <div>
+        <i class="fas fa-music"></i>
+        <p class="info">Techno</p>
+        <p class="info">/ Tech-House</p>
+        <p class="info">/ Industrial</p>
+      </div>
+
+      <div>
+        <i class="fas fa-calendar"></i>
+        <!--Start date - End date-->
+        <p class="info">12 apr. 2020 - 13 apr. 2020</p>
+      </div>
+      <div>
+        <i class="fas fa-clock"></i>
+        <!--Start time - End time-->
+        <p class="info">12.00 - 02.00</p>
+      </div>
+      <div>
+        <i class="fas fa-map-marker-alt"></i>
+        <a
+          target="_blank"
+          href="https://www.google.nl/maps/place/Herengracht+433,+1017+BR+Amsterdam"
+          class="info"
+        >
+        <!--Location with link to google maps ^-->
+        Herengracht 433, 1017 BR Amsterdam</a>
+      </div>
+
+      <h6 class="heading">Lineup</h6>
+      <!-- For all artists -->
+      <div class="artist">
+        <img
+          src="../assets/blank-profile-picture-973460_1280-1-e1546851120685.png"
+          class="artistPicture"
+        />
+        <p class="artistName">Artist Name</p>
+      </div>
     </div>
-    <h6 class="heading">Info</h6>
-    <div><i class="fas fa-calendar"></i><p class="info">12 apr. 2020 - 13 apr. 2020</p></div>
-    <div><i class="fas fa-clock"></i><p class="info">12.00 - 02.00</p></div>
-    <div><i class="fas fa-map-marker-alt"></i>
-    <a target="_blank" href="https://www.google.nl/maps/place/Herengracht+433,+1017+BR+Amsterdam" class="info">Herengracht 433, 1017 BR Amsterdam</a></div>
-     
-    
   </div>
 </template>
 
 <script>
+export default {
+    methods: {
+        //Function to attend/unattend festival
+        change: function(){
+            var elem = document.getElementById("btn2");
+    if (elem.value=="Ik ga") elem.value = "Aanwezig!";
+    else elem.value = "Ik ga";
+        }
+    },
+    mounted () {
+        
+    },
+    created () {
+    
+    }
+}
 </script>
 
 <style scoped>
+.backIcon {
+  margin-top: 18px;
+}
+.innercontent {
+  height: 100%;
+  overflow: auto;
+}
+
+.innercontent::-webkit-scrollbar {
+  display: none;
+}
+
 .festivalImage {
   margin-top: 18px;
   width: 100%;
@@ -81,7 +119,7 @@
 
 .btn {
   display: inline;
-  width: 120px;
+  width: 150px;
   height: 45px;
   text-align: center;
   background: #065bff;
@@ -143,19 +181,64 @@ h6 {
   text-align: center;
 }
 
-.info{
-    margin-left: 5px;
-display: inline;
-font-family: Nunito;
-font-style: normal;
-font-weight: normal;
+.info {
+  margin-left: 5px;
+  display: inline;
 }
 
-i{
-    width: 16px;
+i {
+  width: 16px;
 }
 
-a{
-    color: black;
+a {
+  color: black;
+  text-decoration: underline;
+  font-family: Nunito;
+  font-style: normal;
+  font-weight: normal;
+}
+
+p {
+  margin-bottom: 0px;
+  font-family: Nunito;
+  font-style: normal;
+  font-weight: normal;
+}
+
+.genre {
+  display: inline;
+  margin-right: 5px;
+}
+
+.artist {
+  background: white;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  width: auto;
+  padding: 5px 0;
+  margin-bottom: 9px;
+}
+
+.artistPicture {
+  display: inline-block;
+  margin-left: 10px;
+  height: 25px;
+  width: 25px;
+  border-radius: 100%;
+}
+
+.artistName {
+  display: inline-block;
+  margin-left: 10px;
+}
+
+.artistTime {
+  line-height: 15px;
+  margin-left: 10px;
+  font-size: 10px;
+  display: inline-block;
+  overflow: hidden;
+  max-width: 30px;
+  height: 20px;
 }
 </style>
