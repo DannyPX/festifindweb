9d0171c11fea78cadbef1307dd28532771e3a421
DannyPX <Damnoga@hotmail.com>
2020-05-27 13:15:40 +0200

    Fix login type

--- src/views/LoginNormal.vue
+++ src/views/LoginNormal.vue
@@ -4,7 +4,7 @@
     <form>
       <div class="form-group">
         <label for="exampleInputEmail1">Email</label>
-        <input type="password" class="form-control" id="exampleInputEmail1" :value="credentials.email" @input="updateEmail">
+        <input type="text" class="form-control" id="exampleInputEmail1" :value="credentials.email" @input="updateEmail">
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">Wachtwoord</label>
