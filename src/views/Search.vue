d624d8049b185628c081d1ea23625bd82606fb40
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-06-09 11:03:31 +0200

    Update Search.vue

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -25,19 +25,7 @@
 </template>
 
 <script>
-new Vue({
-  el: "#app",
-  data() {
-    return {
-      info: null,
-    };
-  },
-  mounted() {
-    axios
-      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then((response) => (this.info = response));
-  },
-});
+
 </script>
 
 <style></style>
